b54b3ad6242dcb8b72d13cf67802bcc3ecfd3ee0
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-07-13 17:18:33 -0300

    fix(Script): fix menu script

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-const buttons = document.querySelectorAll("button");
+const button = document.querySelector("#burger-menu");
 const nav = document.querySelector("#nav");
 
-buttons.forEach((button) => {
-	button.addEventListener("click", () => {
-		const currentState = button.getAttribute("data-state");
+button.addEventListener("click", () => {
+	const currentState = button.getAttribute("aria-expanded");
 
-		if (!currentState || currentState === "closed") {
-			button.setAttribute("data-state", "opened");
-			button.setAttribute("aria-expanded", "true");
-			nav.classList.add("menu-mobile");
-		} else {
-			button.setAttribute("data-state", "closed");
-			button.setAttribute("aria-expanded", "false");
-			nav.classList.remove("menu-mobile");
-		}
-	});
+	if (!currentState || currentState === "false") {
+		button.setAttribute("aria-expanded", "true");
+		nav.classList.add("menu-mobile");
+	} else {
+		button.setAttribute("aria-expanded", "false");
+		nav.classList.remove("menu-mobile");
+	}
 });
